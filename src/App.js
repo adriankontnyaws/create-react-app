2d4884d4c6dd467b1767a53ab61f8b7adae9866b
Adrian Kontny <adrian.kontny@outlook.com>
2019-12-08 17:05:58 +0100

    Test AWS github hooks 002

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test React
+          Test React 003
         </a>
       </header>
     </div>
