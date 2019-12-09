1f27505e74603be103a15bfa4712d406ac1aab80
Adrian Kontny <adrian.kontny@outlook.com>
2019-12-09 14:34:27 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test React 007
+          Test React 008
         </a>
       </header>
     </div>
